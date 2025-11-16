63cc0046f2666e9e118fa654b1afc9da5a3c3434
Elias de Jong <elias@fastserial.com>
2025-11-16 14:46:58 +0100

    Add Lite3

--- json/go/go.mod
+++ json/go/go.mod
@@ -1,24 +1,25 @@
 module benchmark
 
-go 1.23.0
+go 1.24.0
 
 require (
 	benchmarks/common v0.0.0-00010101000000-000000000000
-	github.com/bytedance/sonic v1.14.0
+	github.com/bytedance/sonic v1.14.1
 	github.com/goccy/go-json v0.10.5
 	github.com/json-iterator/go v1.1.12
 	github.com/willabides/rjson v0.2.0
 )
 
 require (
+	github.com/bytedance/gopkg v0.1.3 // indirect
 	github.com/bytedance/sonic/loader v0.3.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/klauspost/cpuid/v2 v2.3.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	golang.org/x/arch v0.20.0 // indirect
-	golang.org/x/sys v0.35.0 // indirect
+	golang.org/x/arch v0.22.0 // indirect
+	golang.org/x/sys v0.37.0 // indirect
 )
 
 replace benchmarks/common => ../../common/go
---
+++ json/test_lite3.c
@@ -0,0 +1,66 @@
+#include <libnotify.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <lite3.h>
+
+
+#ifdef __clang__
+#define COMPILER "clang"
+#else
+#define COMPILER "gcc"
+#endif
+
+
+int main() {
+	size_t bufsz = 1024*1024*256;
+	size_t buflen;
+	unsigned char *buf = malloc(bufsz);
+	if (!buf) {
+		perror("Failed to allocate memory");
+		return 1;
+	}
+	
+	printf("converting doc to Lite3\n");
+	if (lite3_json_dec_file(buf, &buflen, bufsz, "/tmp/1.json") < 0)
+		goto error;
+	notify_with_pid("C/" COMPILER " (lite3)");
+
+	lite3_iter iter;
+	size_t coords_ofs;
+	if (lite3_get_arr(buf, buflen, 0, "coordinates", &coords_ofs) < 0)
+		goto error;
+	if (lite3_iter_create(buf, buflen, coords_ofs, &iter) < 0)
+		goto error;
+	
+	int count = 0;
+	double sum_x = 0.0, sum_y = 0.0, sum_z = 0.0;
+	double tmp_x, tmp_y, tmp_z;
+	size_t ofs;
+	while (lite3_iter_next(buf, buflen, &iter, NULL, &ofs) == LITE3_ITER_ITEM) {
+		if (lite3_get_f64(buf, buflen, ofs, "x", &tmp_x) < 0
+		    || lite3_get_f64(buf, buflen, ofs, "y", &tmp_y) < 0
+		    || lite3_get_f64(buf, buflen, ofs, "z", &tmp_z) < 0) {
+			goto error;
+		}
+		sum_x += tmp_x;
+		sum_y += tmp_y;
+		sum_z += tmp_z;
+		++count;
+	}
+	const double inv = 1.0 / count;
+	const double avg_x = sum_x * inv;
+	const double avg_y = sum_y * inv;
+	const double avg_z = sum_z * inv;
+
+	notify("stop");
+	printf("Coordinate {\"x\": %e, \"y\": %e, \"z\": %e}\n", avg_x, avg_y, avg_z);
+
+	free(buf);
+	return 0;
+error:
+	perror("Error running benchmark");
+	free(buf);
+	return 1;
+}
